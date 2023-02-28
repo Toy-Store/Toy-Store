6c2fd55dbc0cde850ceae559ea94410bb30577ad
Ldtkhang <tkth0306@gmail.com>
2023-02-28 21:29:31 +0700

    created ProductController and repo

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/product")
+ */
+class ProductController extends AbstractController
+{
+    private ProductRepository $repo;
+    public function __construct(ProductRepository $repo)
+   {
+      $this->repo = $repo;
+   }
+
+}
