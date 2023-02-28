6dc8226b04dc8148289d17938feb8449584e431a
Ldtkhang <tkth0306@gmail.com>
2023-02-28 21:25:22 +0700

    Create a build function for the product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -26,6 +26,9 @@ class Product
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $image = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $category = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +78,18 @@ class Product
     public function setImage(?string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
