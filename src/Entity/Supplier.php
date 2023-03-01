d30bb772ca1267c2a8c25ca9edfeef05f9d717e5
vuanh166161 <anhnvgcc200163@fpt.edu.vn>
2023-03-01 10:21:18 +0700

    created entity and controller Supplier

---
+++ src/Entity/Supplier.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SupplierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SupplierRepository::class)]
+class Supplier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $suppliername = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $address = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSuppliername(): ?string
+    {
+        return $this->suppliername;
+    }
+
+    public function setSuppliername(?string $suppliername): self
+    {
+        $this->suppliername = $suppliername;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+}
